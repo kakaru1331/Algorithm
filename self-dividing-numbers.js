6e617d1a9b75b76dc748130857b4c4dcf2b49949
kakaru <kakaru1331@gmail.com>
2018-04-23 17:31:54 +0900

    04-23 problem-solving

---
+++ self-dividing-numbers.js
@@ -0,0 +1,26 @@
+var selfDividingNumbers = function(left, right) {
+    var numArray = [];
+    var str = '';
+    var strLeng = 0;
+    
+    for (currentNum=left; currentNum <= right; currentNum++) {
+        str = currentNum.toString();       
+        
+        if (str.indexOf('0') != -1) {
+            continue;
+        }
+        
+        strLeng = str.length;
+        for (var i=0; i < strLeng; i++) {
+            let digit = parseInt(str[i]);            
+            
+            if (currentNum % digit != 0)
+                break;
+            
+            if (i == strLeng-1)
+                numArray.push(currentNum);
+        }       
+    }
+    
+    return numArray;
+};
