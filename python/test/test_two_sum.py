9527296e4b22e73db71071cf7c70b5cfa0ec47bb
kakaru <kakaru1331@gmail.com>
2020-10-02 15:43:48 +0900

    CLN: apply snake case on files and functions

---
+++ python/test/test_two_sum.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.two_sum import two_sum
+
+class TestTwoSum(unittest.TestCase):
+
+    def test_two_sum(self):
+        mockArray = [2,7,11,15]
+        target = 9
+        expecetedIndices = [0, 1]
+
+        self.assertEqual(two_sum(self, mockArray, target), expecetedIndices, "It should return a list contains 0 and 1")
