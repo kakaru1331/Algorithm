c331134e388fa005d74418eacf112f2c761c5757
kakaru <kakaru1331@gmail.com>
2020-12-08 17:56:00 +0900

    TST: add a test suite of distribute-candies

---
+++ js/tests/solutions/distribute-candies.spec.ts
@@ -0,0 +1,10 @@
+import { distributeCandies } from '@solutions/distribute-candies'
+
+test('distributeCandies', () => {
+  const candies = [1,1,2,2,3,3]
+
+  const typesOfCandies = distributeCandies(candies)
+  const expected = 3
+
+  expect(typesOfCandies).toBe(expected)
+})
