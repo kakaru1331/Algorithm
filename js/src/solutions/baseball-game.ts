8d5e768e4eee96fd85ae65ebb770693d538cfc5a
kakaru <kakaru1331@gmail.com>
2020-12-08 17:54:11 +0900

    CLN: lint baseball-game

--- js/src/solutions/baseball-game.ts
+++ js/src/solutions/baseball-game.ts
@@ -3,35 +3,35 @@
  * @param ops
  * @returns result after calculate points
  */
-function calPoints(ops: Array<string>): number {
-    let sum = 0;
-    const validPoints = [];
-    let curIdx = -1;
-    let validPoint = 0;
-    
-    for (let i=0; i < ops.length; i++) {        
-        if (ops[i] == 'C') {
-            sum -= validPoints[curIdx];
-            validPoints.pop();
-            curIdx--;
-        } else if (ops[i] == 'D') {
-            validPoint = validPoints[curIdx];
-            sum += validPoint*2;
-            validPoints.push(validPoint*2);
-            curIdx++;
-        } else if (ops[i] == '+') {
-            validPoint = validPoints[curIdx] + validPoints[curIdx-1];
-            sum += validPoint;
-            validPoints.push(validPoint);
-            curIdx++;
-        } else {
-            validPoints.push(parseInt(ops[i]));
-            curIdx++;
-            sum += validPoints[curIdx];
-        }
+function calPoints (ops: Array<string>): number {
+  let sum = 0
+  const validPoints = []
+  let curIdx = -1
+  let validPoint = 0
+
+  for (let i = 0; i < ops.length; i++) {
+    if (ops[i] === 'C') {
+      sum -= validPoints[curIdx]
+      validPoints.pop()
+      curIdx--
+    } else if (ops[i] === 'D') {
+      validPoint = validPoints[curIdx]
+      sum += validPoint * 2
+      validPoints.push(validPoint * 2)
+      curIdx++
+    } else if (ops[i] === '+') {
+      validPoint = validPoints[curIdx] + validPoints[curIdx - 1]
+      sum += validPoint
+      validPoints.push(validPoint)
+      curIdx++
+    } else {
+      validPoints.push(parseInt(ops[i]))
+      curIdx++
+      sum += validPoints[curIdx]
     }
-    
-    return sum;
+  }
+
+  return sum
 }
 
 export { calPoints }
