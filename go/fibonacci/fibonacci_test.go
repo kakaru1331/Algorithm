d703392dbdff68de07674a14061301d26325bf29
kakaru <kakaru1331@gmail.com>
2021-01-28 22:49:34 +0900

    test: add test suites of fibonacci

---
+++ go/fibonacci/fibonacci_test.go
@@ -0,0 +1,17 @@
+package fibonacci
+
+import (
+	"testing"
+)
+
+func TestPrintFibonacci(t *testing.T) {
+	n := 10
+
+	PrintFibonacci(n)
+}
+
+func TestPrintFib(t *testing.T) {
+	n := 5
+
+	PrintFib(n)
+}
